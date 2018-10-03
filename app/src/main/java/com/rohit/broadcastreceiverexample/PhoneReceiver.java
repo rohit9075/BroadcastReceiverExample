5620918a13ca2331142b0c285efa322b023b08d7
Rohit Kumar <rohit9075@gmail.com>
2018-10-03 16:46:22 +0530

    broadcast receiver comment

--- app/src/main/java/com/rohit/broadcastreceiverexample/PhoneReceiver.java
+++ app/src/main/java/com/rohit/broadcastreceiverexample/PhoneReceiver.java
@@ -12,17 +12,22 @@ import android.widget.Toast;
 public class PhoneReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        // bundle object
         Bundle extras = intent.getExtras();
         if (extras != null) {
 
+            // getting the phone call state
             String state = extras.getString(TelephonyManager.EXTRA_STATE);
-            Log.d("PhoneReceiver", state);
-            Toast.makeText(context, state, Toast.LENGTH_SHORT).show();
 
+            Log.d("PhoneReceiver", state);
+
+            // checking phone state for ringing
             if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+
+                // if phone is ringing, Phone number is saving to variable
                 String phoneNumber = extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
                 Log.d("PhoneReceiver", phoneNumber);
-                Toast.makeText(context, phoneNumber, Toast.LENGTH_SHORT).show();
             }
 
         }
